82c47d441c6f04f8cfe36a9c755cca3993921a85
Chuks-onah Benedict <48535419+itachi1010@users.noreply.github.com>
2023-03-15 21:15:19 +0000

    i've added login functionality

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -17,15 +17,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 #change it later
-
 from django.contrib import admin
-from django.urls import path
-from core.views import index, contact
+from django.urls import path,include
 
 
 urlpatterns = [
-    path('contact/',contact, name='contact'),
-    path('',index, name='index'),
+    path('', include('core.urls')),
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
